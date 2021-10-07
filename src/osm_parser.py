2cb2ae3b07a87c118d2fc9468f8e1c6f65f0e555
David Lange <53982746+Davomat@users.noreply.github.com>
2021-10-07 11:36:34 +0200

    substituted logger and simplified input logic

--- src/osm_parser.py
+++ src/osm_parser.py
@@ -1,24 +1,16 @@
-import logging
 import sys
 
 from core.parser import Parser
 
-
 if __name__ == '__main__':
-    log = logging.getLogger("__name__")
     if len(sys.argv) < 3:
-        log("You need to specify an input and an output file!")
-
+        raise AttributeError("You need to specify an input and an output file!")
     if sys.argv[1] == sys.argv[2]:
-        log("the input file and the output file must not be the same!")
-    else:
-        parser = Parser(sys.argv[1])
-        door_to_door = False
-        simplify_ways = False
-        remove_deadends = False
-        if '-dd' in sys.argv or 'dd' in sys.argv:
-            door_to_door = True
-        if '-sw' in sys.argv or 'sw' in sys.argv:
-            simplify_ways = True
-        parser.find_ways(simplify_ways, door_to_door)
-        parser.write_osm(sys.argv[2])
+        raise AttributeError("The input file and the output file must not be the same!")
+
+    parser = Parser(sys.argv[1])
+    door_to_door = '-dd' in sys.argv or 'dd' in sys.argv
+    simplify_ways = '-sw' in sys.argv or 'sw' in sys.argv
+    remove_deadends = False
+    parser.find_ways(simplify_ways, door_to_door)
+    parser.write_osm(sys.argv[2])
