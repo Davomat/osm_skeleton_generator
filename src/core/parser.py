13d4cca73ddcc36c6cdc9e246c1a8cdae0701cab
David Lange <53982746+Davomat@users.noreply.github.com>
2021-10-28 16:13:22 +0200

    added xml beautifier into parser

--- src/core/parser.py
+++ src/core/parser.py
@@ -1,5 +1,6 @@
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
 # TODO: replace with better Library ET doesn't ignore WhiteSpace --> 'xyz' != ' xyz '
+from bs4 import BeautifulSoup
 
 from core.connection import *
 from core.room import *
@@ -11,13 +12,13 @@ class Parser:
 
     Args
     ----
-    file : str
-        Defines the name (with absolute or relative path) of the osm file that shall be parsed.
+    file_name : str
+        Defines the name (with absolute or relative path) of the osm file_name that shall be parsed.
 
     Attributes
     ----------
     rooms : list[Room]
-        All rooms the can be found in the given osm file.
+        All rooms the can be found in the given osm file_name.
     connections : list[Connection]
         The representations of all doors as points.
     doors : dict[str, list[tuple[float, float]]]
@@ -25,22 +26,22 @@ class Parser:
     ways : list[dict[str, Union[list[tuple[float, float]], str]]]
         The calculated ways with their type and level information.
     nodes : dict[str, dict[tuple[float, float], int]]
-        POIs with their level and id information for the osm file.
+        POIs with their level and id information for the osm file_name.
     """
 
-    def __init__(self, file: str):
+    def __init__(self, file_name: str):
         self.rooms = []
         self.connections = []
         self.doors = {}
         self.ways = []
         self.nodes = {}
-        self.read_data(file)
+        self.read_data(file_name)
 
-    def read_data(self, file: str):
+    def read_data(self, file_name: str):
         """
-        Parses the given file and collects information about important doors, rooms, and connections.
+        Parses the given file_name and collects information about important doors, rooms, and connections.
         """
-        tree = et.parse(file)
+        tree = ET.parse(file_name)
         root = tree.getroot()
 
         # parse nodes
@@ -63,12 +64,12 @@ class Parser:
                 members, con_type = parse_connection(element, root)
                 self.connections.append(Connection(members, con_type))
 
-            elif element.find("tag[@v='multipolygon']") is not None:  # Multipolygone finden
+            elif element.find("tag[@v='multipolygon']") is not None:  # find multipolygons
                 polygon, level, barriers = parse_multipolygon(element, root)
                 if polygon is not None:
                     self.rooms.append(Room(polygon, level, barriers))
 
-        # remove duplicate rooms (--> Multipolygon)
+        # remove duplicate rooms (--> multipolygon)
         for room in self.rooms:
             for room2 in self.rooms:
                 if room.level == room2.level and room.polygon == room2.polygon and room != room2:
@@ -88,17 +89,17 @@ class Parser:
         for connection in self.connections:
             self.ways += connection.find_ways(self.doors)
 
-    def write_osm(self, file: str):
+    def write_osm(self, file_name: str, beautify: bool):
         """
-        Creates a new file with the given name in OSM format to save the calculates ways for navigation.
+        Creates a new file_name with the given name in OSM format to save the calculates ways for navigation.
         """
-        osm_root = et.Element("osm", version='0.6', upload='false')
+        osm_root = ET.Element("osm", version='0.6', upload='false')
         processed = {}
         osm_node_id = -2
         osm_way_id = -2
         osm_ways = []
         for way in self.ways:
-            osm_way = et.Element("way", id=str(osm_way_id))
+            osm_way = ET.Element("way", id=str(osm_way_id))
             osm_way_id -= 1
             level = way['level']
             if ';' in level:  # way connecting two levels --> only two nodes
@@ -113,7 +114,7 @@ class Parser:
                 if way['way'][0] not in self.nodes[levels[0]]:
                     self.nodes[levels[0]][way['way'][0]] = osm_node_id
                     osm_node_id -= 1
-                    et.SubElement(osm_root, "node", id=str(self.nodes[levels[0]][way['way'][0]]),
+                    ET.SubElement(osm_root, "node", id=str(self.nodes[levels[0]][way['way'][0]]),
                                   lat=str(way['way'][0][0]),
                                   lon=str(way['way'][0][1]))
                     processed[levels[0]].append(way['way'][0])
@@ -121,13 +122,13 @@ class Parser:
                 if way['way'][1] not in self.nodes[levels[1]]:
                     self.nodes[levels[1]][way['way'][1]] = osm_node_id
                     osm_node_id -= 1
-                    et.SubElement(osm_root, "node", id=str(self.nodes[levels[1]][way['way'][1]]),
+                    ET.SubElement(osm_root, "node", id=str(self.nodes[levels[1]][way['way'][1]]),
                                   lat=str(way['way'][1][0]),
                                   lon=str(way['way'][1][1]))
                     processed[levels[1]].append(way['way'][1])
 
-                et.SubElement(osm_way, "nd", ref=str(self.nodes[levels[0]][way['way'][0]]))
-                et.SubElement(osm_way, "nd", ref=str(self.nodes[levels[1]][way['way'][1]]))
+                ET.SubElement(osm_way, "nd", ref=str(self.nodes[levels[0]][way['way'][0]]))
+                ET.SubElement(osm_way, "nd", ref=str(self.nodes[levels[1]][way['way'][1]]))
 
             else:
                 if level not in self.nodes:
@@ -138,17 +139,48 @@ class Parser:
                         if node not in self.nodes[level]:
                             self.nodes[level][node] = osm_node_id
                             osm_node_id -= 1
-                        et.SubElement(osm_root, "node", id=str(self.nodes[level][node]), lat=str(node[0]),
+                        ET.SubElement(osm_root, "node", id=str(self.nodes[level][node]), lat=str(node[0]),
                                       lon=str(node[1]))
                         processed[level].append(node)
-                    et.SubElement(osm_way, "nd", ref=str(self.nodes[way['level']][node]))
+                    ET.SubElement(osm_way, "nd", ref=str(self.nodes[way['level']][node]))
 
-            et.SubElement(osm_way, "tag", k="indoor", v="yes")
-            et.SubElement(osm_way, "tag", k="level", v=way['level'])
-            et.SubElement(osm_way, "tag", k="highway", v=way['type'])
+            ET.SubElement(osm_way, "tag", k="indoor", v="yes")
+            ET.SubElement(osm_way, "tag", k="level", v=way['level'])
+            ET.SubElement(osm_way, "tag", k="highway", v=way['type'])
             osm_ways.append(osm_way)
 
         for osm_way in osm_ways:
             osm_root.append(osm_way)
-        tree = et.ElementTree(osm_root)
-        tree.write(file, encoding='utf-8', xml_declaration=True)
+
+        tree = ET.ElementTree(osm_root)
+        tree.write(file_name, encoding='utf-8', xml_declaration=True)
+
+        if beautify:
+            self._beautify(file_name)
+
+    def _beautify(self, file_name: str):
+        """
+        Takes the generated file_name and inserts newlines as well as indentation.
+        """
+        # open xml file_name and parse it with BeautifulSoup
+        with open(file_name, 'r') as file:
+            soup: str = BeautifulSoup(file, "lxml-xml").prettify()
+
+        # re-build all lines with doubled indentation space
+        lines = []
+        for line in soup.splitlines(keepends=True):
+            lines.append(self._preceding_spaces(line) * " " + line)
+
+        # re-open xml file_name and write modified lines
+        with open(file_name, 'w') as file:
+            file.writelines(lines)
+
+    @staticmethod
+    def _preceding_spaces(line: str) -> int:
+        """
+        Calculate the number of preceding spaces in a string.
+        """
+        counter = 0
+        while counter < len(line) and line[counter] == ' ':
+            counter += 1
+        return counter
--- src/osm_parser.py
+++ src/osm_parser.py
@@ -5,13 +5,14 @@ from core.parser import Parser
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        raise AttributeError("You need to specify an input and an output file!")
+        raise AttributeError("You need to specify an input and an output file_name!")
     if sys.argv[1] == sys.argv[2]:
-        raise AttributeError("The input file and the output file must not be the same!")
+        raise AttributeError("The input file_name and the output file_name must not be the same!")
 
     parser = Parser(sys.argv[1])
-    door_to_door = '-dd' in sys.argv or 'dd' in sys.argv
-    simplify_ways = '-sw' in sys.argv or 'sw' in sys.argv
-    remove_deadends = False
+    beautify_xml = '-bf' in sys.argv
+    door_to_door = '-dd' in sys.argv
+    simplify_ways = '-sw' in sys.argv
+    remove_dead_ends = False
     parser.find_ways(simplify_ways, door_to_door)
-    parser.write_osm(sys.argv[2])
+    parser.write_osm(sys.argv[2], beautify_xml)
